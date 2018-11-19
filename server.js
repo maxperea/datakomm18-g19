8a312a1a80c9e02d1d08f8eaae6c60e56bd734fe
maxperea <maxpereaduring@gmail.com>
2018-11-19 10:28:01 +0100

    lägger till allt egentligen

---
+++ server.js
@@ -0,0 +1,142 @@
+// Dependencies
+let movement = require('./movement')
+let express = require('express')
+let http = require('http')
+let path = require('path')
+let socketIO = require('socket.io')
+
+let app = express()
+let server = http.Server(app)
+let io = socketIO(server)
+
+let port = 5000
+let refreshRate = 1000 / 60
+
+app.set('port', port)
+app.use('/static', express.static(__dirname + '/static'))
+
+// Routing
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+// Starts the server.
+server.listen(port, () => {
+    console.log(`Starting server on ${port}`)
+})
+
+// Setup global variables
+// The object containing all player objects
+let players = {}
+
+// The object containing all item objects
+let world = {}
+
+//The object describing the size of the gamespace
+let bounds = {
+    left: 10,
+    up: 10,
+    right: 790,
+    down: 590
+}
+
+// The state of the game: the combination of all players and the world
+let state = {
+    players: players,
+    world: world
+}
+
+// Variables used by later functions, speed: movement of the world, trigger: accumulates speed to
+// trigger the addition of a new row, counter: counts up by makeRow() to identify the different rows
+let speed = -0.2
+let trigger = 0
+let counter = 0
+
+// Uses global variables counter and world to add new rows to the game $world
+let makeRow = () => {
+    x = Math.floor(Math.random()*700)
+    world[counter] = {
+        left: 0,
+        up: 600,
+        right: x,
+        down: 650
+    }
+    counter++
+    world[counter] = {
+        left: x + 100,
+        up: 600,
+        right: 800,
+        down: 650
+    }
+    counter++
+}
+
+
+
+// All the functions reacting on messages from clients
+io.on('connection', socket => {
+    // Creates a new player object on arrival
+    socket.on('new player', () => {
+        players[socket.id] = {
+            x:300,
+            y:300,
+            yspeed: 0,
+            xspeed: 0,
+            radius: 10,
+            onground: false,
+            score: 0,
+            lost: false
+        }
+    })
+    // Takes keyboard data and applies movePlayer function, moving the player.
+    // FIX: right now the score is emitted here in lack of better way, needs to be changed
+    socket.on('movement', data => {
+        let player = players[socket.id] || {};
+        movement.movePlayer(player, data, 5, bounds)
+        socket.emit('score', player.score)
+    })
+    // Removes the player on disconnect
+    socket.on('disconnect', function() {
+        delete players[socket.id]
+    });
+})
+
+
+
+// This function updates the gamestate and then emits it to all players
+setInterval(() => {
+    // Applies forces, checks loss and confines the player, it also checks collision with items
+    for (var i in players) {
+        let player = players[i]
+        movement.checkLoss(player, bounds)
+        movement.forces(player)
+        movement.confine(player, bounds)
+        for (var id in world) {
+            let item = world[id]
+            movement.collisionWThing(player, item)
+        }
+    }
+    // Moves the world upwards with speed $speed
+    for (var id in world) {
+        let item = world[id]
+        movement.moveItemUp(speed, item)
+    }
+
+    // Here the trigger is increased by $speed and when it reaches 100 makeRow() is called
+    // Score is also added to the player
+    trigger += -speed
+    if (trigger > 100) {
+        trigger = 0
+        makeRow()
+        //speed -= 0.05
+        for (var id in players) {
+            let player = players[id]
+            if(!player.lost) {
+                player.score -= 100 * speed
+            }
+        }
+    }
+
+    // State is emitted
+    io.sockets.emit('state', state)
+}, refreshRate)
---
+++ static/game.js
@@ -0,0 +1,87 @@
+// Setup global variables, add io
+let socket = io()
+// The current state of intended movement
+let movement = {
+    left: false,
+    up: false,
+    right: false,
+    down: false
+}
+
+// Get canvas from html document
+let canvas = document.getElementById('canvas')
+// Set params
+canvas.width = 800
+canvas.height = 600
+// Get context from canvas (this is the variable used to draw)
+var context = canvas.getContext('2d')
+
+// Send new player message
+socket.emit('new player')
+
+// Add event listener for key presses, changes the movement object
+document.addEventListener('keydown', event => {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = true
+            break
+        case 87: // W
+            movement.up = true
+            break
+        case 68: // D
+            movement.right = true
+            break
+        case 83: // S
+            movement.down = true
+            break
+    }
+})
+document.addEventListener('keyup', event => {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = false
+            break
+        case 87: // W
+            movement.up = false
+            break
+        case 68: // D
+            movement.right = false
+            break
+        case 83: // S
+            movement.down = false
+            break
+    }
+})
+
+
+// Sends information of movement every 1/60th second
+setInterval( ()=> {
+    socket.emit('movement', movement)
+}, 1000 / 60)
+
+// When a new state is received, draw it
+socket.on('state', state => {
+    context.fillStyle = 'black'
+    context.fillRect(0, 0, 800, 600)
+    context.fillStyle = 'yellow'
+    for(let id in state.players) {
+        let player = state.players[id]
+        if (!player.lost) {
+            context.beginPath();
+            context.arc(player.x, player.y, 10, 0, 2 * Math.PI)
+            context.fill()
+        }
+    }
+    for (let id in state.world) {
+        let item = state.world[id]
+        context.fillStyle = 'purple'
+        context.beginPath();
+        context.rect(item.left, item.up, item.right - item.left, item.down - item.up)
+        context.fill()
+    }
+})
+
+// When new score is received, update score
+socket.on('score', score => {
+    document.getElementById('score').innerText = score;
+})
